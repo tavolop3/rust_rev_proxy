3ffe0f7c6526bd99082e078dddd10c285ce82adf
tavo <tavo.lop33@gmail.com>
2026-02-11 22:55:25 -0300

    fix structure

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,16 +1,17 @@
-use std::io::{ Read, Write };
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
-const SERVER_ADDR: &str = "127.0.0.1:9090"; 
+const SERVER_ADDR: &str = "127.0.0.1:9090";
 
 fn main() {
-     let listener = TcpListener::bind(SERVER_ADDR).unwrap(); 
-     for stream in listener.incoming() {
-         match stream {
-             Ok(mut stream) => {
+    let listener = TcpListener::bind(SERVER_ADDR).unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
                 let mut buffer = [0u8; 1024];
                 loop {
                     let bytes_received = match stream.read(&mut buffer) {
+                        Ok(0) => panic!("Proxy ended connection"),
                         Ok(bytes_received) => bytes_received,
                         Err(err) => {
                             println!("Error while reading from proxy: {err:?}");
@@ -19,10 +20,11 @@ fn main() {
                     };
                     let data_received = &buffer[..bytes_received];
                     println!("R:{data_received:?}");
-                    let _ = stream.write_all(&mut buffer);
+                    let _ = stream.write_all(data_received);
+                    let _ = stream.flush();
                 }
-             },
-             Err(err) => println!("Error: {err:?}"),
-         }
-     }
+            }
+            Err(err) => println!("Error: {err:?}"),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{ Read, Write };
-use std::net::{ TcpListener, TcpStream };
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 const PROXY_ADDR: &str = "127.0.0.1:8080";
 const SERVER_ADDR: &str = "127.0.0.1:9090";
@@ -14,18 +14,20 @@ fn main() {
             Ok(mut cli_stream) => {
                 let mut buffer = [0u8; MAX_SIZE_BUFF];
                 let bytes_received = match cli_stream.read(&mut buffer) {
+                    Ok(0) => break,
                     Ok(bytes_received) => bytes_received,
                     Err(err) => {
                         println!("Error while reading from client: {err:?}");
                         break;
                     }
                 };
-                
+
                 let cli_data_received = &buffer[..bytes_received];
-                match server_stream.write_all(&cli_data_received) {
+                match server_stream.write_all(cli_data_received) {
                     Ok(_) => {
                         let mut buffer = [0u8; MAX_SIZE_BUFF];
                         let bytes_received = match server_stream.read(&mut buffer) {
+                            Ok(0) => panic!("Server disconnected"),
                             Ok(bytes_received) => bytes_received,
                             Err(err) => {
                                 println!("Error while reading from server: {err:?}");
@@ -33,27 +35,22 @@ fn main() {
                             }
                         };
                         let server_data_received = &buffer[..bytes_received];
-                        if let Err(err) = cli_stream.write_all(&server_data_received) {
-                                println!("Error while sending server data to the client: {err:?}");
-                                break;
+                        if let Err(err) = cli_stream.write_all(server_data_received) {
+                            println!("Error while sending server data to the client: {err:?}");
+                            break;
                         };
-                    },
+                        let _ = cli_stream.flush();
+                    }
 
                     Err(err) => {
                         println!("Error while sending client data to the server: {err:?}");
                         break;
                     }
                 }
-                
-                                
-            },
+            }
             Err(err) => {
                 println!("Connection failed: {err:?}")
             }
         }
     }
-
-
-    
 }
-
