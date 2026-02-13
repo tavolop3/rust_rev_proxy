977c67a60e6b66a99ac3b9bf797e0e6f7fafc030
tavo <tavo.lop33@gmail.com>
2026-02-13 10:48:26 -0300

    partial writes and backpressure implemented

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,7 @@ use std::thread;
 
 const PROXY_ADDR: &str = "127.0.0.1:8080";
 const SERVER_ADDR: &str = "127.0.0.1:9090";
-
-struct ConnectionData {
-    cli_stream: TcpStream,
-    serv_stream: TcpStream, // First version will use an individual server socket for each cli connection
-    cli_to_serv_buff: Vec<u8>,
-    serv_to_cli_buff: Vec<u8>,
-}
+const BUFFER_SIZE: usize = 4096; // 4 KiB
 
 fn main() {
     let cli_listener = TcpListener::bind(PROXY_ADDR).expect("Failed to bind proxy address");
@@ -27,66 +21,86 @@ fn main() {
     }
 }
 
-fn handle_cli_connection(cli_stream: TcpStream) {
+fn handle_cli_connection(mut cli_stream: TcpStream) {
+    let mut cli_buffer = [0u8; BUFFER_SIZE];
+    let mut serv_buffer = [0u8; BUFFER_SIZE];
+    let mut cli_offset: usize = 0;
+    let mut cli_data_len: usize = 0;
+    let mut serv_offset: usize = 0;
+    let mut serv_data_len: usize = 0;
+
     cli_stream
         .set_nonblocking(true)
         .expect("Failed to set nonblocking cli stream");
 
-    let serv_stream = TcpStream::connect(SERVER_ADDR).expect("Failed to connect to server"); // just panic for this version
+    let mut serv_stream = TcpStream::connect(SERVER_ADDR).expect("Failed to connect to server"); // just panic for this version
     serv_stream
         .set_nonblocking(true)
         .expect("Failed to set nonblocking server stream");
 
-    let mut conn_data = ConnectionData {
-        cli_stream: cli_stream,
-        serv_stream: serv_stream,
-        cli_to_serv_buff: vec![],
-        serv_to_cli_buff: vec![],
-    };
-
     loop {
-        // TODO: limit read
-        // read cli data
-        let mut temp_read_arr = [0u8; 4096]; // Temporary space
-        let bytes_received = match conn_data.cli_stream.read(&mut temp_read_arr) {
-            Ok(0) => {
-                println!("Client disconnected");
-                break;
-            }
-            Ok(n) => n,
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => 0,
-            Err(e) => {
-                println!("Error while reading from client: {e:?}");
-                break;
-            }
-        };
+        // Offset represents where unsent data starts in the buffer
 
-        // write cli data to server only if cli data was sent
-        if bytes_received > 0 {
-            conn_data.cli_to_serv_buff = temp_read_arr[..bytes_received].to_vec();
-            if let Err(e) = conn_data.serv_stream.write(&conn_data.cli_to_serv_buff) {
-                println!("Error while sending client data to the server: {e:?}");
-                break;
+        // cli data -> server
+        if cli_offset != BUFFER_SIZE {
+            match serv_stream.write(&cli_buffer[cli_offset..cli_data_len]) {
+                Ok(n) => {
+                    cli_offset += n;
+                    if cli_offset == cli_data_len {
+                        cli_offset = 0;
+                        cli_data_len = 0;
+                    }
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {}
+                Err(e) => println!("Error while sending client data to the server: {e:?}"),
             }
         }
 
-        // read server data
-        let bytes_received = match conn_data.serv_stream.read(&mut temp_read_arr) {
-            Ok(0) => panic!("Server disconnected"),
-            Ok(n) => n,
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => 0,
-            Err(err) => {
-                println!("Error while reading from server: {err:?}");
-                break;
+        // server data -> cli
+        if serv_offset != BUFFER_SIZE {
+            match cli_stream.write(&serv_buffer[serv_offset..serv_data_len]) {
+                Ok(n) => {
+                    serv_offset += n;
+                    if serv_offset == serv_data_len {
+                        serv_offset = 0;
+                        serv_data_len = 0;
+                    }
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {}
+                Err(e) => println!("Error while sending server data to the client: {e:?}"),
             }
-        };
+        }
 
-        // write server data to cli only if server sent data
-        if bytes_received > 0 {
-            conn_data.serv_to_cli_buff = temp_read_arr[..bytes_received].to_vec();
-            if let Err(err) = conn_data.cli_stream.write(&conn_data.serv_to_cli_buff) {
-                println!("Error while sending server data to the client: {err:?}");
-                break;
+        // read cli data
+        if cli_data_len != BUFFER_SIZE {
+            match cli_stream.read(&mut cli_buffer[cli_data_len..BUFFER_SIZE]) {
+                Ok(0) => {
+                    println!("Client disconnected");
+                    break;
+                }
+                Ok(n) => {
+                    cli_data_len += n;
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {}
+                Err(e) => {
+                    println!("Error while reading from client: {e:?}");
+                }
+            };
+        }
+
+        // read server data
+        if serv_data_len != BUFFER_SIZE {
+            match serv_stream.read(&mut serv_buffer[serv_data_len..BUFFER_SIZE]) {
+                Ok(0) => {
+                    panic!("Server disconnected");
+                }
+                Ok(n) => {
+                    serv_data_len += n;
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {}
+                Err(e) => {
+                    println!("Error while reading from server: {e:?}");
+                }
             };
         }
     }
